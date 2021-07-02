832b0245a1b9c5ad7b29fd89b669ed5f9a5d00a8
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2021-07-02 16:55:15 -0400

    add função geral

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ function consumoHora(potencia){
 
 //recebe o valor da conta e a quantidade de kw/h consumido na mesma conta
 function precoKwHora(valorConta, qtdKwhConsumidos){
-	return valorConta/qtdKwConsumidos;
+	return valorConta/qtdKwhConsumidos;
 }
 
 //recebe o valor do kw/h e o kw/h consumido por hora e retorna o valor do consumo por hora em valor monetário
@@ -16,4 +16,14 @@ function consumoPorHora(precoKwHora, consumoHora){
 //recebe o valor do kw/h e a quantidade de horas que o dispositivo fica ligado e retorna o gasto em reais
 function gastoTotalEletronico(qtdHorasLigado, consumoPorHora){
 	return qtdHorasLigado*consumoPorHora;
+}
+
+/* recebe o valor da potência do dispositivo, valor da ultima conta, quantidade de kw/h consumidos e quantas horas esse dispositivo fica ligado  
+e retorna o gasto total estimado do eletrônico em reais */
+function gastoTotalEletronico(potencia, valorUltimaConta, qtdKwhConsumidos, qtdHorasLigado){
+	let consumoHora = potencia/1000;
+	let precoKwHora = valorUltimaConta/qtdKwhConsumidos;
+	let consumoPorHora = precoKwHora*consumoHora;
+
+	return qtdHorasLigado * consumoPorHora;
 }
