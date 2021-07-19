e174b2e0147500b5c091b8c94db967072616094f
SteveCraft13 <anamarciaoliveira76@outlook.com>
2021-07-19 18:51:16 -0400

    Site conectado com o javascript

--- functions.js
+++ functions.js
@@ -27,3 +27,43 @@ function gastoTotalEletronico(potencia, valorUltimaConta, qtdKwhConsumidos, qtdH
 
 	return qtdHorasLigado * consumoPorHora;
 }
+
+/* recebe o valor da potência do dispositivo, valor da ultima conta, quantidade de kw/h consumidos e quantas horas esse dispositivo fica ligado  
+e retorna o gasto total estimado do eletrônico em reais e exibe na tela */
+function gastoTotalEletronicoTela(){
+
+	const potencia = document.getElementById("potencia").value;
+	const valorUltimaConta = document.getElementById("valorUltimaConta").value; 
+	const qtdKwhConsumidos = document.getElementById("qtdKwhConsumidos").value;
+	const qtdHorasLigado = document.getElementById("qtdHorasLigado").value;
+
+	
+
+	let consumoHora = potencia/1000;
+	let precoKwHora = valorUltimaConta/qtdKwhConsumidos;
+	let consumoPorHora = precoKwHora*consumoHora;
+
+	
+
+	const resultado = qtdHorasLigado * consumoPorHora;
+	
+
+	const resultadoTela = document.getElementById("resultadoTela");
+
+	resultadoTela.innerHTML = "R$" + resultado.toFixed(2);
+
+
+	const formulario = document.getElementById("formulario");
+	const resultadoDiv = document.getElementById("resultado");
+
+	formulario.style.display = 'none';
+	resultadoDiv.style.display = 'block';
+}
+
+function novoCalculoDisplay(){
+	const formulario = document.getElementById("formulario");
+	const resultadoDiv = document.getElementById("resultado");
+
+	formulario.style.display = 'block';
+	resultadoDiv.style.display = 'none';
+}
